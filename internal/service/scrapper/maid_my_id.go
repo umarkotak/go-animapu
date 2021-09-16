0e5bdcfd55cb9b1d05b8b94149ed450029699404
umar.ramadhana <umar.ramadhana@gmail.com>
2021-09-16 16:04:15 +0700

    fix maid my scraper

--- internal/service/scrapper/maid_my_id.go
+++ internal/service/scrapper/maid_my_id.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"github.com/gocolly/colly"
+	"github.com/sirupsen/logrus"
 	"github.com/umarkotak/go-animapu/internal/models"
 )
 
@@ -19,30 +20,40 @@ func ScrapMaidMyHomePage() models.MangaDB {
 	mangaDatas := map[string]*models.MangaData{}
 
 	maxWeight := 1000000
-	idx := 0
 
-	c.OnHTML(".flexbox3-content", func(e *colly.HTMLElement) {
-		imageLink := e.ChildAttr("img", "src")
-		mangaLink := e.ChildAttr("a", "href")
+	c.OnHTML("body > main > div > div.container > div.flexbox4 > div", func(e *colly.HTMLElement) {
+		imageLink := e.ChildAttr("div.flexbox4-content a div.flexbox4-thumb img", "src")
+		mangaCompactTitle := e.ChildAttr("div.flexbox4-content a div.flexbox4-thumb img", "title")
+
+		mangaLink := e.ChildAttr("div.flexbox4-content a", "href")
 		splittedLink := strings.Split(mangaLink, "/manga/")
 		mangaTitle := splittedLink[1]
 		mangaTitle = strings.ReplaceAll(mangaTitle, "/", "")
 
-		weight := maxWeight - idx
-		idx += 1
+		mangaLastChapterRaw := e.ChildText("div > div > ul > li:nth-child(1) > a")
+		mangaLastChapterRaw = strings.ReplaceAll(mangaLastChapterRaw, "Ch.", "")
+		mangaLastChapterRaw = strings.ReplaceAll(mangaLastChapterRaw, " ", "")
+		mangaLastChapter, err := strconv.ParseFloat(mangaLastChapterRaw, 64)
+		if err != nil {
+			logrus.Error(mangaLastChapterRaw, err)
+			mangaLastChapter = 0
+		}
 
 		tempMangaData := models.MangaData{
-			CompactTitle:     mangaTitle,
-			MangaLastChapter: 150,
-			AveragePage:      150,
+			Title:            mangaTitle,
+			CompactTitle:     mangaCompactTitle,
+			MangaLastChapter: int(mangaLastChapter),
+			AveragePage:      100,
 			ImageURL:         imageLink,
 			Status:           "ongoing",
-			Weight:           weight,
+			Weight:           maxWeight,
 		}
+		fmt.Printf("%+v\n", tempMangaData)
 
 		_, ok := mangaDatas[mangaTitle]
 		if !ok {
 			mangaDatas[mangaTitle] = &tempMangaData
+			maxWeight--
 		}
 	})
 
@@ -108,18 +119,32 @@ func ScrapMaidMyMangaDetailPage(title string) models.MangaDetail {
 		Title: title,
 	}
 
+	maxChapter := int64(0)
 	c.OnHTML(".flexch-infoz", func(e *colly.HTMLElement) {
 		tempChapterLink := e.ChildAttr("a", "href")
 		tempChapterLinkArr := strings.Split(tempChapterLink, "www.maid.my.id")
 		chapterLink := tempChapterLinkArr[1]
 		chapterLink = strings.ReplaceAll(chapterLink, "/", "")
 
-		tempChapterNo := e.ChildText("span.ch")
-		tempChapterNoArr := strings.Split(tempChapterNo, " ")
-		chapterNo := tempChapterNoArr[1]
+		tempChapterNo := e.ChildText("span")
+		tempSuffix := e.ChildText("span span")
+		tempChapterNo = strings.ReplaceAll(tempChapterNo, tempSuffix, "")
+		tempChapterNos := strings.Split(tempChapterNo, " ")
+		lastChapterNoString := "0"
+		if len(tempChapterNos) == 2 {
+			lastChapterNoString = tempChapterNos[1]
+		}
+
+		lastChapterNoInt, _ := strconv.ParseFloat(lastChapterNoString, 64)
+		if lastChapterNoInt > float64(maxChapter) {
+			maxChapter = int64(lastChapterNoInt)
+			mangaDetail.LastChapter = fmt.Sprintf("%v", maxChapter)
+		}
 
 		mangaDetail.ChapterLinks = append(mangaDetail.ChapterLinks, chapterLink)
-		mangaDetail.Chapters = append(mangaDetail.Chapters, chapterNo)
+		mangaDetail.Chapters = append(mangaDetail.Chapters, lastChapterNoString)
+		mangaDetail.ChaptersInt = append(mangaDetail.ChaptersInt, int64(lastChapterNoInt))
+		mangaDetail.LastChapterInt = maxChapter
 	})
 
 	c.OnHTML(".series-thumb", func(e *colly.HTMLElement) {
@@ -148,6 +173,10 @@ func ScrapMaidMyMangaDetailPage(title string) models.MangaDetail {
 		genres := strings.Join(tempGenresClean, ", ")
 
 		mangaDetail.Genres = genres
+	})
+
+	c.OnHTML("body > main > div > div > div.container > div > div.series-flexright > div.series-title > h2", func(e *colly.HTMLElement) {
+		mangaDetail.CompactTitle = e.Text
 	})
 
 	c.Visit(fmt.Sprintf("%v/manga/%v/", maidMyHost, title))
